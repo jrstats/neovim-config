e4bf8d467d4c615fbc820c4be90989624c7bd242
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-03-18 17:42:29 +0000

    add save/quit session

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,4 @@ vim.keymap.set("n", "<Leader>tp", ":vsp term://bash<CR>ivenv<CR>ipython3<CR>", {
 vim.api.nvim_set_var("sendtowindow_use_defaults", 0)
 vim.api.nvim_set_keymap("n", "<Leader>pl", "<Plug>SendRight<CR>", {})
 vim.api.nvim_set_keymap("x", "<Leader>pl", "<Plug>SendRightV<CR>", {})
+vim.api.nvim_set_keymap("n", "<Leader>qw", ":mksession!<CR>:qa<CR>", { desc = "Save the session, then quit." })
