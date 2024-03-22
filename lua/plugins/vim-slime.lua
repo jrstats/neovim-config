cfa35c1192660fcac1441af5b0336631d73f00be
James Robinson <james@ember-climate.org>
2024-03-22 18:34:01 +0000

    add vim-slime-cells

--- lua/plugins/vim-slime.lua
+++ lua/plugins/vim-slime.lua
@@ -4,9 +4,10 @@ return {
     keys = {
       { "<C-C>c", "<Plug>SlimeSendCell<BAR>/^# %%<CR>", desc = "Slime Send Cell" },
       { "<C-C><C-C>", "<Plug>SlimeParagraphSend<CR>", desc = "Slime Send Paragraph" },
+      { "<C-C>i", "<Esc>o# %%<CR><Esc>c$", desc = "Create new cell" },
     },
     config = function()
-      vim.g.slime_cell_delimiter = "# %%"
+      vim.g.slime_cell_delimiter = "^# %%"
       vim.g.slime_target = "tmux"
       vim.g.slime_bracketed_paste = 1 -- If using IPython
       vim.g.slime_default_config = {
@@ -14,6 +15,16 @@ return {
         target_pane = "{right-of}", -- Adjust based on your tmux layout
       }
       vim.g.slime_dont_ask_default = 1 -- Don't ask for config every time
+    end,
+  },
+  {
+    "klafyvel/vim-slime-cells",
+    keys = {
+      { "<C-C>j", "<Plug>SlimeCellsNext", desc = "Go to next cell" },
+      { "<C-C>k", "<Plug>SlimeCellsPrev", desc = "Go to previous cell" },
+    },
+    config = function()
+      vim.g.slime_cell_delimiter = "^# %%"
     end,
   },
 }
