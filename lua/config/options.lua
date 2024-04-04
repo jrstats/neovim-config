0c5300295edb1ab1a6f6f9884aa0e13a079570e3
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-04-04 11:43:18 +0100

    add session options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
+local opt = vim.opt
+
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
 -- python
 vim.g.lazyvim_python_lsp = "ruff_lsp"
+
+-- sessionoptions
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
