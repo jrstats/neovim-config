e6b7e79055847cf54ec9a01f99bc17cd3a48b1c9
James Robinson <33688964+jrstats@users.noreply.github.com>
2024-04-08 18:00:09 +0100

    add search for dotenv files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,19 @@ return {
   keys = {
     -- add a keymap to browse plugin files
     {
+      "<leader>fd",
+      function()
+        require("telescope.builtin").find_files({
+          prompt_title = "Search for dotenv files",
+          cwd = vim.fn.getcwd(),
+          find_command = { "rg", "--files", "--glob", ".env*" },
+          file_ignore_patterns = {},
+          no_ignore = true,
+        })
+      end,
+      desc = "Search for dotenv files",
+    },
+    {
       "<leader>fp",
       function()
         require("telescope.builtin").find_files({
@@ -12,7 +25,7 @@ return {
           no_ignore = true,
         })
       end,
-      desc = "Seach for playground files",
+      desc = "Search for playground files",
     },
   },
 }
