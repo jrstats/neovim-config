1e87450ace76d8aa361e672074ce9ecb21a4e333
James Robinson <james@ember-climate.org>
2024-04-19 14:45:37 +0100

    replace LspRestart with LspStop and LspStart

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,4 @@
 vim.api.nvim_set_keymap("n", "<Leader>qw", ":mksession!<CR>:qa<CR>", { desc = "Save the session, then quit." })
 vim.api.nvim_set_keymap("n", "Q", "@qj", { desc = "Apply the 'q' macro and move to the next line" })
 vim.api.nvim_set_keymap("n", "J", "Vj", { desc = "Select next line in visual block mode" })
+vim.api.nvim_set_keymap("n", "<Leader>cL", ":LspStop<CR>:LspStart<CR>", { desc = "Restart the LSP" })
