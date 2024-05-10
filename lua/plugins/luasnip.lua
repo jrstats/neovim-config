5104bd51788d5e15ea199216174395b9a64df54e
James Robinson <james@ember-climate.org>
2024-05-11 00:45:14 +0100

    improve snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -31,7 +31,8 @@ return {
   },
   config = function()
     -- load snippets from path/of/your/nvim/config/my-cool-snippets
-    require("luasnip.loaders.from_lua").lazy_load({ paths = { "./lua/snippets" } })
+    -- require("luasnip.loaders.from_lua").lazy_load({ paths = { "./lua/snippets/init.lua" } })
+    require("lua.snippets.init")
   end,
   keys = {
     {
--- lua/snippets/init.lua
+++ lua/snippets/init.lua
@@ -1,5 +1,22 @@
 local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
+local s = ls.s
+local c = ls.choice_node
+local i = ls.insert_node
+local t = ls.text_node
+local f = ls.function_node
+local d = ls.dynamic_node
 
 ls.add_snippets("python", {
-  ls.parser.parse_snippet("duckdb", "import ember_climate.helpers.duckdb as d"),
+  s(
+    "duckdb",
+    fmt(
+      [[import {}.helpers.duckdb as db
+{}]],
+      {
+        c(1, { t("ember_climate"), t("src") }),
+        i(0),
+      }
+    )
+  ),
 })
