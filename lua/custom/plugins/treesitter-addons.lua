6acb0c8f9a5b75876320569595072362358bf557
James Robinson <james@ember-climate.org>
2024-05-31 17:09:01 +0100

    add treesitter-textobjects

---
+++ lua/custom/plugins/treesitter-addons.lua
@@ -0,0 +1,11 @@
+return {
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	deps = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	setup = function()
+		local opts = require("nvim-treesitter").opts
+		require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
+		-- require("nvim-treesitter-textobjects").setup()
+	end,
+}
