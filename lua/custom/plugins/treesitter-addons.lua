2eab704b66bdb23f4edfdcfbeec2394fc5652b2f
James Robinson <james@ember-climate.org>
2024-05-31 17:41:28 +0100

    add treesitter-textobjects/vim-indent-objects

--- lua/custom/plugins/treesitter-addons.lua
+++ lua/custom/plugins/treesitter-addons.lua
@@ -1,11 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	deps = {
-		"nvim-treesitter/nvim-treesitter",
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		deps = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		setup = function()
+			local opts = require("nvim-treesitter").opts
+			require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
+			-- require("nvim-treesitter-textobjects").setup()
+		end,
 	},
-	setup = function()
-		local opts = require("nvim-treesitter").opts
-		require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
-		-- require("nvim-treesitter-textobjects").setup()
-	end,
+	{
+		"michaeljsmith/vim-indent-object",
+	},
 }
