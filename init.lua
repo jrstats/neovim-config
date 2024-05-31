20e15e6200e65bcc72a0314bcd026e00100cddf1
James Robinson <james@ember-climate.org>
2024-05-31 16:57:10 +0100

    add organize imports to format buffer

--- init.lua
+++ init.lua
@@ -635,8 +635,12 @@ require("lazy").setup({
 		lazy = false,
 		keys = {
 			{
-				"<leader>f",
+				"<leader>cf",
 				function()
+					vim.lsp.buf.code_action({
+						context = { only = { "source.organizeImports" } },
+						apply = true,
+					})
 					require("conform").format({ async = true, lsp_fallback = true })
 				end,
 				mode = "",
@@ -783,13 +787,12 @@ require("lazy").setup({
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
 			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("catppuccin-mocha")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
