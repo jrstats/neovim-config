d61e92bd8e34e2c8dcb8057b61cdffc7d15ad65b
James Robinson <james@ember-climate.org>
2024-06-03 12:10:46 +0100

    remove bufferline (temporarily)

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,33 +1,34 @@
-return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		require("bufferline").setup()
-	end,
-	keys = {
-		{ "<S-h>", "<CMD>BufferLineCyclePrev<CR>" },
-		{ "<S-l>", "<CMD>BufferLineCycleNext<CR>" },
-	},
-	options = {
-		show_close_icon = false,
-		diagnostics = "nvim_lsp",
-		max_prefix_length = 8,
-		diagnostics_indicator = function(count, level, diagnostics_dict, context)
-			if context.buffer:current() then
-				return ""
-			end
-			if level:match("error") then
-				return " " .. vim.g.diagnostic_icons.Error
-			elseif level:match("warning") then
-				return " " .. vim.g.diagnostic_icons.Warning
-			end
-			return ""
-		end,
-		custom_filter = function(buf_number, buf_numbers)
-			if vim.bo[buf_number].filetype ~= "oil" then
-				return true
-			end
-		end,
-	},
-}
+return {}
+-- return {
+-- 	"akinsho/bufferline.nvim",
+-- 	version = "*",
+-- 	dependencies = "nvim-tree/nvim-web-devicons",
+-- 	config = function()
+-- 		require("bufferline").setup()
+-- 	end,
+-- 	keys = {
+-- 		{ "<S-h>", "<CMD>BufferLineCyclePrev<CR>" },
+-- 		{ "<S-l>", "<CMD>BufferLineCycleNext<CR>" },
+-- 	},
+-- 	options = {
+-- 		show_close_icon = false,
+-- 		diagnostics = "nvim_lsp",
+-- 		max_prefix_length = 8,
+-- 		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+-- 			if context.buffer:current() then
+-- 				return ""
+-- 			end
+-- 			if level:match("error") then
+-- 				return " " .. vim.g.diagnostic_icons.Error
+-- 			elseif level:match("warning") then
+-- 				return " " .. vim.g.diagnostic_icons.Warning
+-- 			end
+-- 			return ""
+-- 		end,
+-- 		custom_filter = function(buf_number, buf_numbers)
+-- 			if vim.bo[buf_number].filetype ~= "oil" then
+-- 				return true
+-- 			end
+-- 		end,
+-- 	},
+-- }
