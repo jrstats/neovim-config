8a38ba42aae7cedf7730608aee4b1cb9147f0d62
James Robinson <james@ember-climate.org>
2024-06-03 12:11:03 +0100

    add toggleterm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = {
+		direction = "float",
+		open_mapping = [[<C-t>]],
+		insert_mappings = true,
+		terminal_mappings = true,
+		float_opts = {
+			border = "double",
+		},
+	},
+	config = true,
+}
