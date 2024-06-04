27496ee2f380dfbeb23e1c5337444e47b7822842
James Robinson <james@ember-climate.org>
2024-06-04 15:43:39 +0100

    split options; split out treesitter

--- init.lua
+++ init.lua
@@ -157,8 +157,8 @@ vim.opt.scrolloff = 10
 -- Tabs / spaces
 vim.opt.tabstop = 4
 
--- netrw fix
-vim.g.netrw_bufsettings = "noma nomod nonu nowrap ro buflisted"
+-- NOTE: CUSTOM options
+require("custom.options")
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -693,6 +693,9 @@ require("lazy").setup({
 					end
 					return "make install_jsregexp"
 				end)(),
+				config = function()
+					require("luasnip.loaders.from_lua").load({ paths = { "./lua/custom/snippets/" } })
+				end,
 				dependencies = {
 					-- `friendly-snippets` contains a variety of premade snippets.
 					--    See the README about individual language/framework/plugin snippets:
@@ -849,38 +852,6 @@ require("lazy").setup({
 
 			-- ... and there is more!
 			--  Check out: https://github.com/echasnovski/mini.nvim
-		end,
-	},
-	{ -- Highlight, edit, and navigate code
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
-		},
-		config = function(_, opts)
-			-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-			-- Prefer git instead of curl in order to improve connectivity in some environments
-			require("nvim-treesitter.install").prefer_git = true
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup(opts)
-
-			-- There are additional nvim-treesitter modules that you can use to interact
-			-- with nvim-treesitter. You should go explore a few and see what interests you:
-			--
-			--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-			--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		end,
 	},
 
---
+++ lua/custom/options/init.lua
@@ -0,0 +1,8 @@
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.relativenumber = true
+vim.opt.virtualedit = "block"
+vim.opt.termguicolors = true
+
+-- netrw fix
+vim.g.netrw_bufsettings = "noma nomod nonu nowrap ro buflisted"
